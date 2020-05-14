e6998f90cc2eaadc456e9bd5a64d5126260c5bb9
catarina-goncalves <catarinalfgoncalves@gmail.com>
2020-05-15 00:27:26 +0100

    deck class now has remove card function

--- Deck.py
+++ Deck.py
@@ -39,7 +39,7 @@ class Deck:
 
     def __init__(self):
         self.cards = []
-        suits = ["Clubs", "Spades", "Hearts", "Diamonds"] #FIXME ? currently doesnt have a joker
+        suits = ["Clubs", "Spades", "Hearts", "Diamonds"]
         values = {"2":2,"3":3,"4":4,"5":5,"6":6,"7":7,"8":8,"9":9,"10":10,"Jack":11,"Queen":12,"King":13,"Ace":14}
 
         for suit in suits:
