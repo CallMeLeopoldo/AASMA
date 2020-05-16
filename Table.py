9d3875b42faec8d341a38f8c3173be7d3192a6e2
catarina-goncalves <catarinalfgoncalves@gmail.com>
2020-05-16 01:57:04 +0100

    basic game structure is done. still lacks a lot of things though

--- Table.py
+++ Table.py
@@ -1,18 +1,27 @@
 from Deck import Deck
+from Agent import Agent
 
 class Table:
     
-    def __init__(self):
+    def __init__(self, numAgents, bigBlind):
         self.agents = {}
         self.deck = Deck()
         self.pot = 0
         self.tableCards = []
         self.discardPile = []
 
-        #self.smallBlind
-        #self.bigBlind
-        #self.raiseAmmount
-        #self.currentBet
+        self.bigBlind = bigBlind
+        self.smallBlind = round(bigBlind/2)
+        self.raiseAmmount = bigBlind
+        self.betAmmount = bigBlind
+        self.currentBet = 0
+
+        id = 0
+        while id < numAgents:
+            self.agents[id] = Agent(id)
+            id += 1
+
+
 
     ##################################################
     ####                 GETTERS                 #####
@@ -37,28 +46,96 @@ class Table:
         self.discardPile.append(card)
         
     # places one card on the table
-    def dealCardTable(self, agent):
-        card = self.deck.dealCard()
-        self.tableCards.append(card)
+    def dealCardsTable(self, num):
+        n = 0
+        while n < num: 
+            card = self.deck.dealCard()
+            self.tableCards.append(card)
+            n += 1
 
-    # deals one card to agent
-    def dealCardAgent(self, agent):
-        card = self.deck.dealCard()
-        # TODO agent needs to receive card. if agents has a hand then append to card to hand?
+    # deals two cards to agent
+    def dealCardsAgent(self, agent):
+        card1 = self.deck.dealCard()
+        card2 = self.deck.dealCard()
+        # TODO agent needs to receive cards. if agents has a hand then append to cards to hand?
 
     def addToPot(self, amount):
-        self.pot = self.pot + amount
+        self.pot += amount
+    
+    # doubles both bet amount and raise ammount
+    def doubleAmmounts(self):
+        self.raiseAmmount = self.raiseAmmount * 2
+        self.betAmmount = self.betAmmount * 2 
+
+    # TODO: this will be the full betting round
+    def bettingRound(self):
+        pass
+        #move dealer button to next player
+        #send state to agents ?
+        #for a in agents: a.decide ?
+        #add chips to pot
+        #update agents still playing
+
     
     #################################################
     ####            COMMUNICATION               #####
     #################################################
 
+    # TODO
     def sendMessage(self, msg, agent):
         pass
-
+    
+    # TODO
     def receiveMessage(self, msg, agent):
         pass
 
     #################################################
     ####                  GAME                  #####
     #################################################
+    
+    def game(self):
+        # attribute button to first player
+        self.agents[0].hasButton(True)
+
+        # pay small blind and big blind
+        self.agents[1].paySmallBlind()
+        self.agents[2].payBigBlind()
+
+        # deal 2 cards to each agent
+        for a in self.agents:
+            self.dealCardsAgent(a)
+        
+        # pre flop: betting round
+        self.bettingRound()
+
+        #discard card    --- dont remember if its here or not
+        self.discardCard()
+
+        # flop: place 3 cards showing on table
+        self.dealCardsTable(3)
+
+        # flop: betting round
+        self.bettingRound()
+
+        # turn: double bet ammount and raise ammount
+        self.doubleAmmounts()       # não tenho a certeza se isto duplica outra vez para o river ou se se mantém
+
+        # turn: add 1 card showing on table
+        self.dealCardsTable(1)
+
+        # turn: betting round
+        self.bettingRound()
+
+        # river: add 1 card showing on table
+        self.dealCardsTable(1)
+
+        # river: betting round
+        self.bettingRound()
+
+        # showdown
+        #check if there is more than one active player
+        #check who is the winner
+        #attribute pot? idk is this would even make sense bc the game is over and the agents would stop existing now
+        
+
+    # TODO: add hand and dealer button attributes to Agent, add the methods above that refer to agents
